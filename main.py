efd19c60523436b24746b92e5cd828d3dc83de1e
Milan Velle <110469470+mivelRBFA@users.noreply.github.com>
2022-08-22 12:27:19 +0200

    delete code for bigquery insertion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 from datetime import datetime
-#from google.cloud import scheduler_v1
-from google.cloud import bigquery
-import json
 import os
 app = FastAPI()
 
@@ -15,19 +12,7 @@ async def read_root():
     now = datetime.now()
     return {"hello": "world",  "date": now}
 
-@app.get("/bq_insert_timestamp")
-async def read_root():
-    now = datetime.now()
-    my_dict = {'Timestamp': str(now)}
-    row_to_insert = [my_dict]
-    print(row_to_insert)
-    bq_client = bigquery.Client()
-    table = bq_client.get_table("{}.{}.{}".format("rbfa-workshop-sandboxes", "timestamps_milan", "timestamps"))
-    errors = bq_client.insert_rows_json(table, row_to_insert)
-    if errors == []:
-        return {now: "inserted in bigquery table"}
-    else:
-        return {now: "NOT inserted in bigquery table"}
+
 
 
 
