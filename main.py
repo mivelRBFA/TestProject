515cb7eb1759eb47b284ecd1357599d50d9c015f
Milan Velle <milan.velle.ext@rbfa.be>
2022-08-23 12:07:57 +0200

    VE and gitignore

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from datetime import datetime
 from google.cloud import bigquery
-import os
 
 app = FastAPI()
-credential_path = "C:\\Users\mivel.ext\Documents\cred_GCP_serviceacc.json"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 
 @app.get("/")
