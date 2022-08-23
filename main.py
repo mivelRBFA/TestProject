58dded1a1004e3333d4adb35b9ad8b96621c0a23
Milan Velle <milan.velle.ext@rbfa.be>
2022-08-23 11:59:39 +0200

    add virtual env and gitignore

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from datetime import datetime
+#from google.cloud import scheduler_v1
 from google.cloud import bigquery
 import os
+from google.cloud import storage
 
 app = FastAPI()
-credential_path = "C:\\Users\mivel.ext\Documents\cred_GCP_serviceacc.json"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
 
 
 @app.get("/")
@@ -13,8 +14,6 @@ async def read_root():
     now = datetime.now()
     return {"hello": "world",  "date": now}
 
-
-#code to insert timestamp into bigquery table
 @app.get("/bq_insert_timestamp")
 async def read_root():
     now = datetime.now()
@@ -23,10 +22,26 @@ async def read_root():
     bq_client = bigquery.Client()
     table = bq_client.get_table("{}.{}.{}".format("rbfa-workshop-sandboxes", "timestamps_milan", "timestamps"))
     errors = bq_client.insert_rows_json(table, row_to_insert)
-    if errors == []:
+    if not errors:
         return {now: "inserted in bigquery table"}
     else:
-        return {now: "not inserted in bigquery table"}
+        return {now: "NOT inserted in bigquery table"}
 
+
+#VOLG DEZE LINK: https://www.bing.com/videos/search?q=google+cloud+storage+upload+file+in+API&docid=608052629504462532&mid=E4F3473B3320A2994179E4F3473B3320A2994179&view=detail&FORM=VIRE
+@app.get("/file_to_bucket")
+async def read_root():
+    now = datetime.now()
+
+    #new txtfile
+    with open('log_timestamps.txt', 'w') as f:
+        f.write(str(now)+'\n')
+
+    storage_client = storage.Client()
+    my_bucket = storage_client.get_bucket('rbfa-workshop-sandboxes-milanvelle')
+    blob = my_bucket.blob('log_timestamps')
+    file_path = r'C:\Users\mivel.ext\Documents\PyCharm_Projecten\TestProject\log_timestamps'
+    blob.upload_from_filename(file_path)
+    return {"file uploaded to bucket"}
 
 
