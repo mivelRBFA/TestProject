55c666a9aa05fed353402eede99f35ff0b1cddb9
Milan Velle <110469470+mivelRBFA@users.noreply.github.com>
2022-08-25 09:11:07 +0200

    Revert "Branch update entity in datastore to developm"

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from datetime import datetime
 from google.cloud import bigquery
 from google.cloud import storage
-from google.cloud import datastore
-
 
 app = FastAPI()
 
@@ -42,16 +40,6 @@ async def upload_file():
     blob.upload_from_filename(file_path)
     return {"logfile with new timestamp uploaded to bucket"}
 
-@app.get("/update_entity_in_datastore")
-async def update_entity_datastore():
-    now = datetime.now()
-    client = datastore.Client()
-    kind = "Task"
-    name = "Timestamp"
-    timestamp_key = client.key(kind, name)
-    timestamp = datastore.Entity(key=timestamp_key)
-    timestamp["Timestamp"] = str(now)
-    client.put(timestamp)
-    return {"entity updated"}
+
 
 
