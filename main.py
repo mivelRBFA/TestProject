45018d78c4fbee331a8543cf901ec3d04a739b2f
Milan Velle <milan.velle.ext@rbfa.be>
2022-08-29 09:50:07 +0200

    add isort

--- main.py
+++ main.py
@@ -8,23 +8,19 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     now = datetime.now()
-    return {"hello": "world",  "date": now}
+    return {"hello": "world", "date": now}
 
 
-#code to insert timestamp into bigquery table
+# code to insert timestamp into bigquery table
 @app.get("/bq_insert_timestamp")
 async def read_root():
     now = datetime.now()
-    my_dict = {'Timestamp': f"The Timestamp is {now}"}
+    my_dict = {"Timestamp": f"The Timestamp is {now}"}
     row_to_insert = [my_dict]
     bq_client = bigquery.Client()
-    table = bq_client.get_table('rbfa-workshop-sandboxes.timestamps_milan.timestamps')
+    table = bq_client.get_table("rbfa-workshop-sandboxes.timestamps_milan.timestamps")
     errors = bq_client.insert_rows_json(table, row_to_insert)
     if errors == []:
         return {f"{now} inserted in bigquery table"}
     else:
         return {f"{now} not inserted in bigquery table"}
-
-
-
-
