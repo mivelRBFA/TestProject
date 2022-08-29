2b48df3da84ed3a0cee31a5bb5472f0b856b7a52
Milan Velle <milan.velle.ext@rbfa.be>
2022-08-29 11:23:26 +0200

    update main, add pre-commit-config

--- main.py
+++ main.py
@@ -1,45 +1,41 @@
-from fastapi import FastAPI
 from datetime import datetime
-from google.cloud import bigquery
-from google.cloud import storage
+
+from fastapi import FastAPI
+from google.cloud import bigquery, storage
 
 app = FastAPI()
+
 
 @app.get("/")
 async def read_root():
     now = datetime.now()
-    return {"hello": "world",  "date": now}
+    return {"hello": "world", "date": now}
 
 
-#code to insert timestamp into bigquery table
+# code to insert timestamp into bigquery table
 @app.get("/bq_insert_timestamp")
-async def insert_timestamp():
+async def read_root():
     now = datetime.now()
-    my_dict = {'Timestamp': f"(now)"}
+    my_dict = {"Timestamp": f"The Timestamp is {now}"}
     row_to_insert = [my_dict]
     bq_client = bigquery.Client()
-    table = bq_client.get_table("{}.{}.{}".format("rbfa-workshop-sandboxes", "timestamps_milan", "timestamps"))
+    table = bq_client.get_table("rbfa-workshop-sandboxes.timestamps_milan.timestamps")
     errors = bq_client.insert_rows_json(table, row_to_insert)
     if errors == []:
-        return {now: "inserted in bigquery table"}
+        return {f"{now} inserted in bigquery table"}
     else:
-        return {now: "not inserted in bigquery table"}
+        return {f"{now} not inserted in bigquery table"}
 
 
 @app.get("/file_to_bucket")
 async def upload_file():
     now = datetime.now()
-    f = open("log.txt", 'a')
-    f.write(str(now)+"\n")
-    print('written')
+    f = open("log.txt", "a")
+    f.write(f"{now}" + "\n")
     f.close()
     storage_client = storage.Client()
-    my_bucket = storage_client.get_bucket('rbfa-workshop-sandboxes-milanvelle')
-    blob = my_bucket.blob('Timestamps/'+str(now))
-    file_path = r'C:\Users\mivel.ext\Documents\PyCharm_Projecten\TestProject\log.txt'
+    my_bucket = storage_client.get_bucket("rbfa-workshop-sandboxes-milanvelle")
+    blob = my_bucket.blob("Timestamps/" + f"{now}")
+    file_path = r"C:\Users\mivel.ext\Documents\PyCharm_Projecten\TestProject\log.txt"
     blob.upload_from_filename(file_path)
     return {"logfile with new timestamp uploaded to bucket"}
-
-
-
-
