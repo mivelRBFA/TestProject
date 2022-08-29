bb5e5da96455f18dc276558cc2f94b8e412f3260
Milan Velle <milan.velle.ext@rbfa.be>
2022-08-29 13:41:38 +0200

    cron

--- create_cron_job.sh
+++ create_cron_job.sh
@@ -1,4 +1,4 @@
-gcloud scheduler jobs create http timestamp_in_BQ_job \
-    --schedule every 5 minutes \
-    --uri "http://myproject/my-url.com" \
+gcloud scheduler jobs create http timestamp_in_BQ_job
+    --schedule "*/5 * * * *"
+    --uri "http://127.0.0.1:8000/file_to_bucket"
     --http-method GET
