10e354d5678f1bbd784abc82e542dc0a47f092bb
Milan Velle <milan.velle.ext@rbfa.be>
2022-08-31 09:32:39 +0200

    divided into multiple files

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ from fastapi import FastAPI
 
 app = FastAPI()
 from src import bq_insert_timestamp, file_to_bucket, home, update_entity
-
-home.read_root()
-bq_insert_timestamp.bq_insert()
-file_to_bucket.upload_file()
-update_entity.update_entity_datastore()
