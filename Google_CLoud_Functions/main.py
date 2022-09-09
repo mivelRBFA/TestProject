2be4ff4d0541a27e402f8e24c96fdd3a15b30c7d
Milan Velle <milan.velle.ext@rbfa.be>
2022-09-09 13:20:00 +0200

    flake8

---
+++ Google_CLoud_Functions/main.py
@@ -0,0 +1,7 @@
+
+def call_all_endpoints(request):
+    #één = 'https://svc-milanvelle-dot-rbfa-workshop-sandboxes.ew.r.appspot.com/bq_insert_timestamp'
+    #twee = 'https://svc-milanvelle-dot-rbfa-workshop-sandboxes.ew.r.appspot.com/file_to_bucket'
+    #drie = 'https://svc-milanvelle-dot-rbfa-workshop-sandboxes.ew.r.appspot.com/update_entity_in_datastore'
+    
+    return 'functions called'
